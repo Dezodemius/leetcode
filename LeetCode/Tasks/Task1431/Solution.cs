bc4caaec2a66dee4526cbe7178092f9c6b66a7dc
Гладков Егор <gladkovyegor@gmail.com>
2025-06-14 19:15:57 +0400

    Solve 1431. V2
    Если вычислять максимум самостоятельно, то будет быстрее (4ms -> ~0ms)

--- LeetCode/Tasks/Task1431/Solution.cs
+++ LeetCode/Tasks/Task1431/Solution.cs
@@ -3,7 +3,11 @@
 public class Solution {
   public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
   {
-    var max = candies.Max();
+    var max = 0;
+    for (var i = 0; i < candies.Length; i++)
+      if (candies[i] >= max)
+        max = candies[i];
+
     var result = new List<bool>(candies.Length);
     for (var i = 0; i < candies.Length; i++)
       result.Add(candies[i] + extraCandies >= max);
