7382c2b680c8991492e50e1f08a2a95029d1bef9
Гладков Егор <gladkovyegor@gmail.com>
2025-06-14 18:49:18 +0400

    Solver 1071. V2

--- LeetCode/Tasks/1071. Greatest Common Divisor of Strings/Solution.cs
+++ LeetCode/Tasks/1071. Greatest Common Divisor of Strings/Solution.cs
@@ -4,29 +4,19 @@ namespace Tasks._1071._Greatest_Common_Divisor_of_Strings;
 
 public class Solution {
   public string GcdOfStrings(string str1, string str2) {
-    if (str1.Length >= str2.Length)
-    {
-      for (int i = 0; i < str2.Length; i++)
-      {
-        var divider = str2.Substring(0, str2.Length - i);
-        if (IsDivider(str1, divider) && IsDivider(str2, divider))
-          return divider;
-      }
-    }
-    else
-    {
-      for (int i = 0; i < str1.Length; i++)
-      {
-        var divider = str1.Substring(0, str1.Length - i);
-        if (IsDivider(str1, divider) && IsDivider(str2, divider))
-          return divider;
-      }
-    }
-    return string.Empty;
-  }
+    if (str1 + str2 != str2 + str1)
+      return string.Empty;
 
-  private bool IsDivider(string str, string divider)
-  {
-    return str.Replace(divider, string.Empty).Length == 0;
+    var l1 = str1.Length;
+    var l2 = str2.Length;
+
+    while (l2 > 0)
+    {
+      var temp = l2;
+      l2 = l1 % l2;
+      l1 = temp;
+    }
+
+    return str1.Substring(0, l1);
   }
 }
