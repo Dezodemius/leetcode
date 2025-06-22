796e47350afdc3b0bb899fcceac52dba76d4b544
Dezodemius <gladkovyegor@gmail.com>
2025-06-22 19:03:58 +0400

    Solve 643. V1

---
+++ Tasks/Task643/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace Tasks.Task643;
+
+public class Solution
+{
+  public double FindMaxAverage(int[] nums, int k)
+  {
+    int currentSum = 0;
+    for (int i = 0; i < k; i++)
+    {
+      currentSum += nums[i];
+    }
+    int maxSum = currentSum;
+
+    for (var i = k; i < nums.Length; i++) {
+      currentSum = currentSum - nums[i - k] + nums[i];
+      if (currentSum > maxSum) {
+        maxSum = currentSum;
+      }
+    }
+
+    return (double)maxSum/k;
+  }
+}
