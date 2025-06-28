710ff4033972433f616de7d62999dd18f0de1979
Dezodemius <gladkovyegor@gmail.com>
2025-06-28 17:33:36 +0400

    solve 1207. V1

--- Tasks/Task1207/Solution.cs
+++ Tasks/Task1207/Solution.cs
@@ -3,6 +3,15 @@
 public class Solution
 {
   public bool UniqueOccurrences(int[] arr) {
+    var a = new Dictionary<int, int>();
+    for (int i = 0; i < arr.Length; i++)
+    {
+      if (a.ContainsKey(arr[i]))
+        a[arr[i]]++;
+      else
+        a[arr[i]] = 1;
+    }
 
+    return a.Values.Count == a.Values.Distinct().Count();
   }
 }
