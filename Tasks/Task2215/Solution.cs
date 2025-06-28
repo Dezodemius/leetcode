9c404d82a11f84549cc95336b590c7aaf8b183b4
Dezodemius <gladkovyegor@gmail.com>
2025-06-28 17:20:29 +0400

    Solve 2215. V1

---
+++ Tasks/Task2215/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace Tasks.Task2215;
+
+public class Solution
+{
+  public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
+    var a = nums1.Except(nums2).ToList();
+    var b = nums2.Except(nums1).ToList();
+
+    return new List<IList<int>> { a, b };
+  }
+}
