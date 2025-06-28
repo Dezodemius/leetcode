73d0bb86cd8c27fba4e3300ce07f51779cfc412d
Гладков Егор <gladkovyegor@gmail.com>
2025-06-28 16:23:33 +0400

    solve 1732 (#6)
    * init
    
    * solve 1732. V1

---
+++ Tasks/Task1732/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace Tasks.Task1732;
+
+public class Solution
+{
+  public int LargestAltitude(int[] gain)
+  {
+    var point = 0;
+    var max = 0;
+    for (int i = 0; i < gain.Length; i++)
+    {
+      point += gain[i];
+      if (point > max)
+        max = point;
+    }
+    return max;
+  }
+}
