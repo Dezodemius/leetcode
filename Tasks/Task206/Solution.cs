7339892aaf06321ddff1c4ee59dbb1c83bbcace0
Dezodemius <gladkovyegor@gmail.com>
2025-06-28 20:30:19 +0400

    solve 206. V1
    1. Объявляем previous = null;
    2. Запускаем цикл с проверкой head на null.
    3. Сохраняем значение head.next во временную переменную.
    4. head.next меняем на previous. Получается, в первом проходе будет [1 -> null]
    5. previous меняем на head
    6. head меняем на temp. Получается, head сократился на один узел.
    7. И так пока head не станет null, потому что мы всё вытащили из него.
    8. Возвращаем previous

---
+++ Tasks/Task206/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace Tasks.Task206;
+
+
+public class ListNode
+{ public int val;
+  public ListNode next;
+
+  public ListNode(int val = 0, ListNode next = null)
+  {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+public class Solution
+{
+  public ListNode ReverseList(ListNode head)
+  {
+    // if (head == null)
+    //   return head;
+
+    ListNode previous = null;
+    while (head != null)
+    {
+      var temp  = head.next;
+      head.next = previous;
+      previous = head;
+      head = temp;
+    }
+
+    return previous;
+  }
+}
