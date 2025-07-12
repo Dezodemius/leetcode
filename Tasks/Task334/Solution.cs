532b13db6876a2b22cf114f63e097816ffb4242d
Dezodemius <gladkovyegor@gmail.com>
2025-07-12 11:38:29 +0400

    Solve 334. V0.5

---
+++ Tasks/Task334/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace Tasks.Task334;
+
+public class Solution
+{
+  public bool IncreasingTriplet(int[] nums)
+  {
+    int i = 0;
+    int j = 1;
+    int k = 2;
+
+    for (int l = 0; l < nums.Length; l++)
+    {
+      bool indexes = i < j && j < k;
+      bool values = nums[i] < nums[j] && nums[j] < nums[k];
+      if (indexes && values)
+        return true;
+      else
+      {
+        i++;
+        j++;
+        k++;
+      }
+      if (k == nums.Length)
+        return false;
+    }
+
+    return false;
+  }
+}
