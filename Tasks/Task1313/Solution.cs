fce39c2c5b8705c8b95f7acb65dc112b6b6b8885
Dezodemius <gladkovyegor@gmail.com>
2025-07-19 11:06:26 +0400

    solve 1313. V1

---
+++ Tasks/Task1313/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Tasks.Task1313;
+
+public class Solution {
+  public int[] DecompressRLElist(int[] nums) {
+    var result = new List<int>();
+    for (int i = 0; i < nums.Length - 1; i += 2)
+    {
+      var freq = nums[i];
+      var val = nums[i + 1];
+      for (int j = 0; j < freq; j++)
+        result.Add(val);
+    }
+
+    return result.ToArray();
+  }
+}
