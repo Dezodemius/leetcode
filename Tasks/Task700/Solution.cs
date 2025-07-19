b6e2da6727a06c9f8ad1908b48808e8a5207029e
Dezodemius <gladkovyegor@gmail.com>
2025-07-19 13:28:32 +0400

    solve 700. V1

--- Tasks/Task700/Solution.cs
+++ Tasks/Task700/Solution.cs
@@ -5,6 +5,17 @@ namespace Tasks.Task700;
 public class Solution {
   public TreeNode SearchBST(TreeNode root, int val)
   {
+    if (root == null)
+      return null;
+
+    if (root.val == val)
+      return root;
+
+    if (root.val > val && root.left != null)
+      return SearchBST(root.left, val);
+    if (root.val < val && root.right != null)
+      return SearchBST(root.right, val);
+
     return null;
   }
 }
