cefe799f8f7d9f1293f3a18899db9a0042c9f526
Dezodemius <gladkovyegor@gmail.com>
2025-07-19 12:45:04 +0400

    solve 872. V1

--- Tasks/Task872/Solution.cs
+++ Tasks/Task872/Solution.cs
@@ -1,6 +1,29 @@
-﻿namespace Tasks.Task872;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TaskUtils;
+
+namespace Tasks.Task872;
 
 public class Solution
 {
-  
+  public bool LeafSimilar(TreeNode root1, TreeNode root2)
+  {
+    var leftLeaves = new List<int>();
+    var rightLeaves = new List<int>();
+    LeafCollector(root1, leftLeaves);
+    LeafCollector(root2, rightLeaves);
+
+    return rightLeaves.SequenceEqual(leftLeaves);
+  }
+
+  private void LeafCollector(TreeNode root, List<int> nodes)
+  {
+    if (root == null)
+      return;
+    if (root.left == null && root.right == null)
+      nodes.Add(root.val);
+
+    LeafCollector(root.left, nodes);
+    LeafCollector(root.right, nodes);
+  }
 }
