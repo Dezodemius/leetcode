9dd3d5bcd858d9cb47cd6a882fb3132c949ae96e
Dezodemius <gladkovyegor@gmail.com>
2025-07-20 13:46:20 +0400

    solve 1456. V1

--- Tasks/Task1456/Solution.cs
+++ Tasks/Task1456/Solution.cs
@@ -4,6 +4,38 @@ public class Solution
 {
   public int MaxVowels(string s, int k)
   {
-    return 0;
+    var array = s.ToCharArray();
+    if (k > array.Length)
+      return 0;
+
+    var res = 0;
+
+    for (int i = 0; i < k; i++)
+    {
+      if (IsVowel(array[i]))
+        res++;
+    }
+    if (res == k)
+      return res;
+
+    var tempRes = res;
+    for (int i = 1; i < array.Length - k + 1; i++)
+    {
+      if (IsVowel(array[i - 1]))
+        tempRes--;
+      if (IsVowel(array[i + k - 1]))
+        tempRes++;
+      if (tempRes > res)
+        res = tempRes;
+      if (res == k)
+        return res;
+    }
+
+    return res;
+  }
+
+  private static bool IsVowel(char x)
+  {
+    return x is 'a' or 'e' or 'i' or 'o' or 'u';
   }
 }
