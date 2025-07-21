0a98080de39cdf89b4ae8e44d87e43a081524206
Dezodemius <gladkovyegor@gmail.com>
2025-07-21 12:32:30 +0400

    solve 1491. V1

---
+++ Tasks/Task1491/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace Tasks.Task1491;
+
+public class Solution {
+  public double Average(int[] salary) {
+    var max = salary.Max();
+    var min = salary.Min();
+    return (salary.Sum() - (max + min)) / (double)(salary.Count() - 2);
+  }
+}
