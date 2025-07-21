6afae84ecc66c5ac8a59cd9f9bd80ea92a9f1b6b
Dezodemius <gladkovyegor@gmail.com>
2025-07-21 12:33:10 +0400

    solve 1491. V2

--- Tasks/Task1491/Solution.cs
+++ Tasks/Task1491/Solution.cs
@@ -1,11 +1,15 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tasks.Task1491;
 
 public class Solution {
   public double Average(int[] salary) {
-    var max = salary.Max();
-    var min = salary.Min();
-    return (salary.Sum() - (max + min)) / (double)(salary.Count() - 2);
+    Array.Sort(salary);
+    var list = new List<int>(salary);
+    list.Remove(list[^1]);
+    list.Remove(list[0]);
+    return list.Average();
   }
 }
