ed827471df875e37c2443e04f3dc95f0b4b6e2d6
Dezodemius <gladkovyegor@gmail.com>
2025-07-21 12:40:24 +0400

    solve 929. V1

---
+++ Tasks/Task929/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Task929;
+
+public class Solution {
+  public int NumUniqueEmails(string[] emails) {
+    var parsedEmails = new HashSet<string>();
+    foreach (var email in emails) {
+      var parts = email.Split('@');
+      var domain = parts.Last();
+      var local = parts[0].Split('+')[0];
+      int index = local.IndexOf('.');
+      while (index != -1)
+      {
+        local = local.Remove(index, 1);
+        index = local.IndexOf('.');
+      }
+
+      var parsedEmail = $"{local}@{domain}";
+      parsedEmails.Add(parsedEmail);
+    }
+    return parsedEmails.Count;
+  }
+}
