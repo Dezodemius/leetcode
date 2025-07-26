7b2199371e378c9af5a554de7538ff6d3d368497
Dezodemius <gladkovyegor@gmail.com>
2025-07-26 11:44:14 +0400

    solve 485. V1
    top speed

---
+++ Tasks/Task485/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace Tasks.Task485;
+
+public class Solution
+{
+  public int FindMaxConsecutiveOnes(int[] nums)
+  {
+    var res = 0;
+    var count = res;
+    foreach (var num in nums)
+    {
+      if (num == 1)
+        count++;
+      else
+      {
+        if (count > res)
+          res = count;
+        count = 0;
+      }
+    }
+    if (count > res)
+      res = count;
+
+    return res;
+  }
+}
