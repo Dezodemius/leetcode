5dc08bce4ee2d80dd9b9096a8ef14520f73b6106
Dezodemius <gladkovyegor@gmail.com>
2025-07-26 12:21:31 +0400

    solve 1137. V1

---
+++ Tasks/Task1137/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Tasks.Task1137;
+
+public class Solution
+{
+  public int Tribonacci(int n)
+  {
+    var list = new List<int>();
+    if (n == 0 )
+      return 0;
+    if (n == 2 || n == 1)
+      return 1;
+
+    list.Add(0);
+    list.Add(0);
+    list.Add(1);
+    for (int i = 2; i <= n; i++)
+    {
+      list.Add(list[i - 2] + list[i - 1] + list[i]);
+    }
+    return list[^1];
+  }
+}
