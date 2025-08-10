c28d4fb9173194f95f5db3a8656c170354761ce4
Dezodemius <gladkovyegor@gmail.com>
2025-08-10 10:42:58 +0400

    solve 1004. V1

--- Tasks/Task1004/Solution.cs
+++ Tasks/Task1004/Solution.cs
@@ -4,6 +4,34 @@ public class Solution
 {
   public int LongestOnes(int[] nums, int k)
   {
-    return 0;
+    var left = 0;
+    var right = 0;
+    var count = 0;
+    var maxLength = 0;
+    
+    for (var i = 0; i < nums.Length; i++)
+    {
+      if (nums[i] == 1 && right < nums.Length)
+        right++;
+      else if (nums[i] == 0)
+      {
+        count++;
+        right++;
+      }
+      if (count > k)
+      {
+        while (count > k)
+        {
+          if (nums[left] == 0)
+            count--;
+          left++;
+        }
+      }
+
+      if (maxLength < right - left)
+        maxLength = right - left;
+    }
+    
+    return maxLength;
   }
 }
